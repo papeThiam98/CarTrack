77603a53d505f999ea16fde4a3a4c1d42c96889d
Abdoulaye Thiam <abdoulayethiam@MacBook-Air-de-Abdoulaye.local>
2024-10-19 01:47:33 +0200

    Test mocha

---
+++ myCarServices/test/handler.test.js
@@ -0,0 +1,14 @@
+const mocha = require('mocha');
+const assert= require('assert');
+const sinon = require('sinon');
+const handler = require('../src/handler')
+
+
+describe('testing hello function', function() {
+    it('should return hello world', async () => {
+        const stub = sinon.stub(handler, 'hello').returns("Hello, world!");
+        const resp = handler.hello();
+        console.log(resp)
+        assert.equal(resp, "Hello, world!");
+    })
+})
