42454c5dcc6fc40fe20f3d9c52aac5f926702c1c
Abdoulaye Thiam <abdoulayethiam@MacBook-Air-de-Abdoulaye.local>
2024-10-19 07:25:58 +0200

    Add linter

--- myCarServices/src/handler.js
+++ myCarServices/src/handler.js
@@ -1,8 +1,7 @@
 // handler.js
 exports.hello = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello, world!" }),
-    };
-  };
-  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello, world!' })
+  }
+}
--- myCarServices/test/handler.test.js
+++ myCarServices/test/handler.test.js
@@ -1,14 +1,13 @@
-const mocha = require('mocha');
-const assert= require('assert');
-const sinon = require('sinon');
+const { describe, it } = require('mocha')
+const assert = require('assert')
+const sinon = require('sinon')
 const handler = require('../src/handler')
 
-
-describe('testing hello function', function() {
-    it('should return hello world', async () => {
-        const stub = sinon.stub(handler, 'hello').returns("Hello, world!");
-        const resp = handler.hello();
-        console.log(resp)
-        assert.equal(resp, "Hello, world!");
-    })
+describe('testing hello function', function () {
+  it('should return hello world', async () => {
+    sinon.stub(handler, 'hello').returns('Hello, world!')
+    const resp = handler.hello()
+    console.log(resp)
+    assert.equal(resp, 'Hello, world!')
+  })
 })
