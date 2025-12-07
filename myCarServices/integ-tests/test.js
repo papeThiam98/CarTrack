abcb41ddb23be8cdb4296871834e79ac4107dc4f
Abdoulaye Thiam <abdoulayethiam@MacBook-Air-de-Abdoulaye.local>
2025-12-07 17:47:37 +0100

    ci/cd test laucnher

---
+++ myCarServices/integ-tests/test.js
@@ -0,0 +1,14 @@
+// test.js
+const { hello } = require('../src/handler');
+
+async function testHello() {
+  try {
+    const event = {};  // simuler un event vide ou personnalisé
+    const result = await hello(event);
+    console.log('Résultat:', result);
+  } catch (error) {
+    console.error('Erreur:', error);
+  }
+}
+
+testHello();
