3c216201f0bb51e57a630d3f1d138b64b80a093b
Abdoulaye Thiam <abdoulayethiam@MacBook-Air-de-Abdoulaye.local>
2025-12-07 18:32:01 +0100

    fix prettier issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,34 +3,32 @@ module.exports = {
     browser: true,
     commonjs: true,
     node: true,
-    es2021: true
+    es2021: true,
   },
   extends: 'standard',
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
-    "prettier/prettier": [
-        "error",
-        {
-          "semi": true,  // Ajoute automatiquement des points-virgules
-          "singleQuote": true, // Utilise des simples quotes
-          "trailingComma": "es5", // Ajoute des virgules de fin là où c'est possible
-          "endOfLine": "auto" // Gère les retours à la ligne en fonction du système d'exploitation
-        }
-      ]
-    }
-}
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true, // Ajoute automatiquement des points-virgules
+        singleQuote: true, // Utilise des simples quotes
+        trailingComma: 'es5', // Ajoute des virgules de fin là où c'est possible
+        endOfLine: 'auto', // Gère les retours à la ligne en fonction du système d'exploitation
+      },
+    ],
+  },
+};
