c696945ece34271cc16dcb74b4cbd967980abc34
Abdoulaye Thiam <abdoulayethiam@MacBook-Air-de-Abdoulaye.local>
2025-12-07 18:48:22 +0100

    hotFix

--- myCarServices/integ-tests/test.js
+++ myCarServices/integ-tests/test.js
@@ -1,9 +1,9 @@
 // test.js
 const { hello } = require('../src/handler')
 
-async function testHello() {
+async function testHello () {
   try {
-    const event = {}  //simuler un event vide ou personnalisé
+    const event = {}
     const result = await hello(event)
     console.log('Résultat:', result)
   } catch (error) {
