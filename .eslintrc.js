0cc6199efcc6b4557bd21af7ab6396041412da39
Abdoulaye Thiam <abdoulayethiam@MacBook-Air-de-Abdoulaye.local>
2025-12-07 18:15:54 +0100

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,16 @@ module.exports = {
     browser: true,
     commonjs: true,
     node: true,
-    es2021: true,
+    es2021: true
   },
   extends: 'standard',
   overrides: [
     {
       env: { node: true },
       files: ['.eslintrc.{js,cjs}'],
-      parserOptions: { sourceType: 'script' },
-    },
+      parserOptions: { sourceType: 'script' }
+    }
   ],
   parserOptions: { ecmaVersion: 'latest' },
-  rules: {},
-};
+  rules: {}
+}
