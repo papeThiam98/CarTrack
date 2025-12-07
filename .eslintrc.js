f03e006e91f9b85096017c2fe2eebf3a428ba847
Abdoulaye Thiam <abdoulayethiam@MacBook-Air-de-Abdoulaye.local>
2025-12-07 18:23:57 +0100

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,29 @@ module.exports = {
   extends: 'standard',
   overrides: [
     {
-      env: { node: true },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: { sourceType: 'script' }
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
   ],
-  parserOptions: { ecmaVersion: 'latest' },
-  rules: {}
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    "prettier/prettier": [
+        "error",
+        {
+          "semi": true,  // Ajoute automatiquement des points-virgules
+          "singleQuote": true, // Utilise des simples quotes
+          "trailingComma": "es5", // Ajoute des virgules de fin là où c'est possible
+          "endOfLine": "auto" // Gère les retours à la ligne en fonction du système d'exploitation
+        }
+      ]
+    }
 }
